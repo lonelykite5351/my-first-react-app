11552d32677fdf0d8113d7dd4e0dda60469da90c
lonelykite5351 <55124083+lonelykite5351@users.noreply.github.com>
2022-02-16 15:17:09 +0800

    Add CSS file and import in component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import ExpenseItem from './components/ExpenseItem'
-import './App.css';
+import logo from "./logo.svg";
+import ExpenseItem from "./components/ExpenseItem";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit <code>src/App.js</code> and save to reload.</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <p>This is visiable</p>
         <a
           className="App-link"
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
-    return <h2>Expense Item component</h2>
+  return (
+    <div className='expense-item'>
+        <div>Feb 16 2022</div>
+        <div className='expense-item__description'>
+            <h2>Car Insurance</h2>
+            <div className='expense-item__price'>$294.68</div>
+        </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
